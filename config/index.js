ae51a530838f4a25eae17cf25477673f33e52ba6
Matt Glover <matthewglover@me.com>
2016-10-11 12:10:15 +0100

    add basic cors support

--- config/index.js
+++ config/index.js
@@ -1,6 +1,16 @@
-const port = process.env.PORT || 3000;
 
-const connectionOptions = { port };
+const port = process.env.PORT || 4000;
+
+const connectionOptions = {
+  port,
+  routes: {
+    cors: {
+      origin: ['http://localhost:3000'],
+      maxAge: '86400',
+    },
+  },
+};
+
 
 module.exports = {
   connectionOptions,
