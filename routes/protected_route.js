bd72911f31d62adaabe581dc8057daf4626a5df5
Matt Glover <matthewglover@me.com>
2016-10-11 16:10:24 +0100

    add protected-route checks

--- routes/protected_route.js
+++ routes/protected_route.js
@@ -1,8 +1,10 @@
 module.exports = {
   method: 'GET',
   path: '/protected',
-  config: { auth: 'jwt' },
+  // config: { auth: 'jwt' },
   handler: (req, reply) => {
+    console.log('Request received on /protected');  // eslint-disable-line
     reply({ data: 'boom you\'re authorised' });
+    console.log('Response issued');  // eslint-disable-line
   },
 };
